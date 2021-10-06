325658ce236505637590a10d05e9f1d96c6701cf
k7ubb <kubota.sou@vega.ocn.ne.jp>
2021-10-06 22:12:04 +0900

    Initial Commit

---
+++ TitechAppCourseNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TitechAppCourseNews
+//
+//  Created by 久保田聡 on 2021/10/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TitechAppCourseNews/TitechAppCourseNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TitechAppCourseNewsApp.swift
+//  TitechAppCourseNews
+//
+//  Created by 久保田聡 on 2021/10/06.
+//
+
+import SwiftUI
+
+@main
+struct TitechAppCourseNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
